5d98df617f3975d91a07ce33647fabc28adf2963
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2021-08-14 01:16:03 -0400

    remove station trading

--- src/routes-market.php
+++ src/routes-market.php
@@ -38,17 +38,6 @@ $app->get('/trade', function ($request, $response, $args) {
     return $this->renderer->render($response, 'trade.phtml', $args);
 });
 
-$app->get('/station-trade', function ($request, $response, $args) {
-    $u = Dirt\User::getUser();
-    if (! $u->isLoggedIn()) {
-        return $response->withStatus(302)
-            ->withHeader('Location', '/login');
-    }
-    $u->setTemplateVars($args);
-
-    return $this->renderer->render($response, 'station-trade.phtml', $args);
-});
-
 $app->get('/insurance', function ($request, $response, $args) {
     $u = Dirt\User::getUser();
     if (! $u->isLoggedIn()) {
